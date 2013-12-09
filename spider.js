f608fc8e308f2353d31a2f6ae234f4d40c0e130d
shidel.dev <joeshidel@gmail.com>
2013-12-08 21:29:07 -0500

    Dev stage 1.

--- spider.js
+++ spider.js
@@ -1,35 +1,54 @@
-var Crawler = require("crawler").Crawler;
+var Crawler = require("crawler").Crawler,
+    AdmZip = require('adm-zip'),
+    fs = require("fs"),
+    zip = new AdmZip(),
+    files = [];
 
 function crawl(response, site) {
+    fs.mkdir("./ding/",function(err){
+        console.log(err);
+    });
+    var c = new Crawler({
+        "maxConnections": 10,
+        //called after a page is crawled
+        "callback": function(error, result, $) {
+            console.log(result["request"]["uri"]);
+            //console.log("PathName: "+result.uri.pathname);
+            if (result) {
+                var page = result.body,
+                    res = page.match(/(src|href)\s*=\s*"([^"]*)"/g);
+                for (var i = 0; i < res.length; i++) {
+                    res[i] = res[i].match(/"(.+)"$/)
+                }
 
-     var c = new Crawler({
-         "maxConnections": 10,
-         //called after a page is crawled
-         "callback": function(error, result, $) {
+                if (res && res.length > 0) {
+                    for (var i = 0; i < res.length; i++) {
+                        //files.push(res[i][1]);
 
-             if (result) {
-                 var page = result.body;
-                 var res = page.match(/ /);
-                 if (res && res.length > 0) {
-                     response.writeHead(200, {
-                         "Content-Type": "text/plain"
-                     });
-                     response.write(page);
-                     response.end();
+                    }
 
-                 }
-             }
+                    //fs.writeFile("","")
+                    zip.addLocalFolder("d/");
+                   
 
-             // $ is a jQuery instance scoped to the server-side DOM of the page
-             /* $("a").each(function(index,a) {
-        console.log(a.href);
-        c.queue(a.href);
-    });*/
-         }
-     });
+                    //zip.writeZip( /*target file name*/ "files.zip");
+                    filename = "testfile.zip"
+                    fs.writeFile(filename, zip.toBuffer(), function(err) {
+                        if (err) throw err;
+                        console.log('It\'s saved!');
+                        response.download(__dirname + '/testfile.zip', "testfile.zip");
+                        //response.write("<a href='/d/"+filename+">Click here to download your file.</a>")
+                        //response.end();
+                    });
 
-     // Queue just one URL, with default callback
-     c.queue(site);
- }
+                }
+            }
+        }
+    });
 
- exports.crawl = crawl;
+
+    // Queue just one URL, with default callback
+    c.queue(site);
+}
+
+exports.crawl = crawl;
