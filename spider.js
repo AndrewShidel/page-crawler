fe71b802fc345011f6782aa7d0afc823f08a08f0
shidel.dev <joeshidel@gmail.com>
2013-12-23 18:38:12 -0500

    improved zipping of files

--- spider.js
+++ spider.js
@@ -2,53 +2,62 @@ var Crawler = require("crawler").Crawler,
     AdmZip = require('adm-zip'),
     fs = require("fs"),
     zip = new AdmZip(),
-    files = [];
+    files = [],
+    first = true,
+    count = 0;
 
 function crawl(response, site) {
-    fs.mkdir("./ding/",function(err){
-        console.log(err);
-    });
-    var c = new Crawler({
-        "maxConnections": 10,
-        //called after a page is crawled
-        "callback": function(error, result, $) {
-            console.log(result["request"]["uri"]);
-            //console.log("PathName: "+result.uri.pathname);
-            if (result) {
-                var page = result.body,
-                    res = page.match(/(src|href)\s*=\s*"([^"]*)"/g);
-                for (var i = 0; i < res.length; i++) {
-                    res[i] = res[i].match(/"(.+)"$/)
-                }
+    fs.mkdir(site, function() {
+        fs.mkdir(site + '/resources', function() {
+            var c = new Crawler({
+                "maxConnections": 10,
+                //called after a page is crawled
+                "callback": function(error, result, $) {
+                    
+                    if (result) {
+                        var page = result.body,
+                            res = page.match(/(src|href)\s*=\s*"([^"]*)"/g);
+                        for (var i = 0; i < res.length; i++) {
+                            res[i] = res[i].match(/"(.+)"$/)[0]
+                        }
 
-                if (res && res.length > 0) {
-                    for (var i = 0; i < res.length; i++) {
-                        //files.push(res[i][1]);
+                        if (res && res.length > 0) {
+                            for (var i = 0; i < res.length; i++) {
+                                //files.push(res[i][1]);
+                                if (res[i].search("http") != -1)
+                                    c.queue(res[i]);
+                                else
+                                    c.queue(result.request.uri.protocal + result.request.uri.host + res[i]);
+                            }
 
+                            var filename;
+
+                            if (count == 0) {
+                                filename = site+"/index.html";
+                            } else {
+                                filename = site+"/resources/"+count + '.' + result.request.uri.path.match(/\.([0-9a-z]+)(?:[\?#]|$)/i)[1];
+                            }
+                            count += 1;
+
+                            //fs.writeFile("","")
+                            //zip.addLocalFolder("d/");
+                            zip.addFile(filename, new Buffer(page), "Something");
+
+                            zip.writeZip( /*target file name*/ "test.zip");
+
+                            response.download(__dirname + '/test.zip', "test.zip");
+
+                        }
                     }
-
-                    //fs.writeFile("","")
-                    zip.addLocalFolder("d/");
-                   
-
-                    //zip.writeZip( /*target file name*/ "files.zip");
-                    filename = "testfile.zip"
-                    fs.writeFile(filename, zip.toBuffer(), function(err) {
-                        if (err) throw err;
-                        console.log('It\'s saved!');
-                        response.download(__dirname + '/testfile.zip', "testfile.zip");
-                        //response.write("<a href='/d/"+filename+">Click here to download your file.</a>")
-                        //response.end();
-                    });
-
                 }
-            }
-        }
-    });
+            });
 
 
-    // Queue just one URL, with default callback
-    c.queue(site);
+            // Queue just one URL, with default callback
+            c.queue(site);
+        })
+
+    })
 }
 
 exports.crawl = crawl;
