61b25e1f654b8c54c3139ed07d16d6cb510761e2
shidel.dev <joeshidel@gmail.com>
2014-01-06 14:11:43 -0500

    merging

--- spider.js
+++ spider.js
@@ -2,19 +2,18 @@ var Crawler = require("crawler").Crawler,
     url = require('url');
 
 function crawl(socket, data) {
-    var term = data[1];
-    var root = data[2],
-        next = true;
-    results = [];
+    var term = new RegExp(data[1], "i"),
+        root = data[2],
+        next = true,
+        results = [];
     console.log('data::' + data)
     var c = new Crawler({
         "maxConnections": 10,
         "callback": function(error, result, $) {
             if ($) {
-                var regex = new RegExp(term, "i");
                 if (result) {
                     var page = result.body;
-                    var res = page.match(regex);
+                    var res = page.match(term);
                     if (res && res.length > 0) {
                         socket.emit('hit', result.request.uri.href);
                     }
@@ -24,9 +23,11 @@ function crawl(socket, data) {
                     $("a").each(function(index, a) {
                         var ref = a.href;
                         if (url.parse(ref).hostname == root) {
+                            console.log(results.indexOf(ref) == -1);
                             if (results.indexOf(ref) == -1) {
                                 c.queue(ref);
                                 results.push(ref);
+                            
                             }
                         }
                     });
@@ -57,7 +58,8 @@ function crawl(socket, data) {
     });
 
     c.queue(data[0]);
-
+    results.push(data[0])
+    console.log('this')
     socket.on('disconnect', function() {
         next = false;
     });
