4e4b0fd040a15ec2ffd9b8d7bd1682e06efe3ae6
shidel.dev <joeshidel@gmail.com>
2014-01-11 18:37:32 -0500

    moved files

--- server.js
+++ server.js
@@ -1,21 +1,49 @@
 var express = require('express'),
-    http = require('http'),
-    url = require("url"),
-    server = http.createServer(function(request, response) {}),
     spider = require("./spider"),
-    site,
-    io = require('socket.io').listen(1234);
+    app = express(),
+    request = require('request'),
+    http = require("http"),
+    fs = require('fs'),
+    url = require('url');
+
+
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('download/', function(req, res) {
+    console.log(req.params[0].substring(1));
+    getSource(req.params[0].substring(1), res);
+});
+
+
+function getSource(uri, res) {
+
+    var d = new Date();
+    var n = d.getTime()
+    n = n + '.html'
+
+    request(uri, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            //res.send(body)
+            fs.writeFile("temp/" + n, body, function(err) {
+                if (err) throw err;
+                res.download('./temp/' + n, "page.html");
+            });
+        }
+    });
+}
+
+io = require('socket.io').listen(1234);
 
 io.sockets.on('connection', function(socket) {
 
     socket.on('submit', function(data) {
         spider.crawl(socket, data);
     });
-   
+
 });
-
-
-
-
 
 //exports.start = start;
